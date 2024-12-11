a6f627407104f3b161cfa08a425eb1f7f80610f0
Dilge Karakaş <dil-kar@hotmail.com>
2024-12-11 13:56:32 +0300

    Workflow adjustments done

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
-# setup.py configuration
+from setuptools import setup, find_packages
+
+setup(
+    name="OptunaFS",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "optuna",
+    ],
+    python_requires=">=3.8",
+)
